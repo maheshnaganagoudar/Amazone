7e31a308b23dc5242271276a32985bb2e89cdcb6
prince.mahi <prince.mahi@Dell>
2017-03-24 16:43:25 +0530

    code changed in amazon

--- Amazon.py
+++ Amazon.py
@@ -14,7 +14,12 @@ from selenium.webdriver import ActionChains
 if __name__ == '__main__':
     
     driver = webdriver.Firefox()
-    driver.get("https://www.irctc.in")
-    driver.find_element_by_xpath("")
+    driver.get("https://www.abhibus.com")
+    driver.find_element_by_xpath(".//*[@id='myModalLay']/div/div[1]/div[2]/span").click()
+    driver.find_element_by_xpath(".//*[@id='source']").send_keys("Bangalore")
+    driver.find_element_by_xpath(".//*[@id='destination']").send_keys("Hubli")
+    driver.find_element_by_xpath(".//*[@id='datepicker1']").click()
+    driver.find_element_by_xpath(".//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[4]/td[6]/a").click()
+    driver.find_element_by_xpath(".//*[@id='roundTrip']/a").click()
     
    
